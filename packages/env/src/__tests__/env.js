63d047c8167b7011268c9b46633d5f4a1abfa3f1
Kyle Bromma <kbromma@gmail.com>
2018-11-23 23:34:23 -0800

    Updated @deuterium/env

--- packages/env/src/__tests__/env.js
+++ packages/env/src/__tests__/env.js
@@ -5,3 +5,7 @@ import env from '../env'
 test('Check that env contains what it should', () => {
     expect(env.NODE_ENV).toBe('test')
 })
+
+test('Check test of environment', () => {
+    expect(env.IN_TEST_ENV).toBeTruthy()
+})
--- packages/env/src/env.js
+++ packages/env/src/env.js
@@ -5,4 +5,10 @@ import read from 'read-env'
 
 const env = read({ transformKey: false })
 
+env.NODE_ENV = env.NODE_ENV ?? 'development'
+
+env.IN_DEV_ENV = env.NODE_ENV === 'development'
+env.IN_PROD_ENV = env.NODE_ENV === 'production'
+env.IN_TEST_ENV = env.NODE_ENV === 'test'
+
 module.exports = env
